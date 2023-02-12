346421b0692511bf908d114ed888c0281c603851
Unforturnacy <tericsimons12@gmail.com>
2023-02-12 18:43:57 -0500

    created profile

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
 
 
 ###
@@ -45,3 +46,11 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',date = format_date_joined("2023-02-12"))
+
+def format_date_joined(date):
+    dt = datetime.strptime(date, "%Y-%m-%d")
+    return dt.strftime('%B, %Y')
